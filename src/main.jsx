9a61f672f1b969d2b7b75aa0ae68a14afb3e89ed
zesanahmed <zesanahmed593@gmail.com>
2023-05-23 18:37:34 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from './error-page';
+import Contact, {
+  loader as contactLoader
+} from './routes/contact';
+import EditContact from "./routes/edit";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
